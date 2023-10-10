04cec0caa0a24c4e2051c9deddcf6d6ebcd72c35
Abdulaziz <abizlancer@gmail.com>
2023-10-10 11:31:49 +0500

    feat: toggle active in categories, toggling popup list and set active sortBy

--- src/components/Categories.component.jsx
+++ src/components/Categories.component.jsx
@@ -1,34 +1,47 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
+
+
 
 const Categories = () => {
-  const categories = [
-    "Все",
-    "Мясные",
-    "Вегетарианская",
-    "Гриль",
-    "Острые",
-    "Закрытые",
-  ];
+  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"];
+  const sortBy = ['популярности', 'по цене', 'по алфавиту']
+
+  const [categoryListActive, setCategoryListActive] = useState(false)
+  const [sortByCategoryActive, setSortByCategory] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0)  
+
+  const setActiveCategory = (index) => {
+    setActiveIndex(index)
+  }
+  const setActiveSortByCategory = (index) => {
+    setSortByCategory(index)
+    // Close popup list
+    categoryListToggle()
+  }
+  const categoryListToggle = () => {
+    setCategoryListActive(!categoryListActive)
+  }
 
   return (
     <Fragment>
       <div className="categories">
-        <div className="categories__items">
+        <ul className="categories__items">
           {categories.map((category, key) => {
-            return <button className={`categories__items--btn ${key == 0 ? 'active' : ''}`} key={key}>{category}</button>;
+            return <li className={`categories__items--btn ${key == activeIndex ? 'active': ''}`} key={key} onClick={() => setActiveCategory(key)}>{category}</li>;
           })}
-        </div>
+        </ul>
         <div className="categories__sort">
             <span>
               <img src="./src/assets/images/chevron-up.svg" alt="chevron-up" />
               Сортировка по:
             </span>
             <div className="categories__sort--popup">
-              <button className="categories__sort--popup-selected">популярности</button>
-              <ul className="categories__sort--popup__list">
-                <li className="active">популярности</li>
-                <li>по цене</li>
-                <li>по алфавиту</li>
+              <button className="categories__sort--popup-selected" onClick={categoryListToggle}>{sortBy[sortByCategoryActive]}</button>
+              <ul className={`categories__sort--popup__list ${categoryListActive ? 'active' : ''}`}>
+                {sortBy.map((item, key) => (
+                  <li className={`${sortByCategoryActive === key ? 'active' : ''}`} onClick={() => setActiveSortByCategory(key)} key={key}>{item}</li>
+                ))}
               </ul>
             </div>
         </div>
